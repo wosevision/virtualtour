8c725981f77a14c12e865d6798bf54ef4c44f224
Wose <Jacksonteather@hotmail.com>
2016-11-21 09:53:07 -0500

    Fixed drilldown scrolling
    Heights stay a minimum size for drilldown scroll

--- app/components/sidebar/drilldown/content/drilldown-content-poi_controller.js
+++ app/components/sidebar/drilldown/content/drilldown-content-poi_controller.js
@@ -10,7 +10,11 @@ class PoiCtrl {
 	}
 	$onInit() {
 
-		const hoverEl = document.getElementById(`link_${this.toRender.scene}`);
+		const hoverEl = document.getElementById(
+			this.toRender.scene 
+			? `link_${this.toRender.scene}`
+			: `hotspot_${this.toRender._id}`
+		);
 		this.$element.on('mouseenter', () => {
 			hoverEl.emit('mouseenter');
 		});
@@ -22,8 +26,13 @@ class PoiCtrl {
 			this.SceneResource.get({ id: this.toRender.scene }, scene => {
 				this.scene = scene;
 			});
+		} else {
+			this.hotspot = this.toRender;
 		}
 
+	}
+	goto({code, parent}) {
+		console.log(code, parent);
 	}
 }
 
