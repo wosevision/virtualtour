661024418ccf0243f285d2c9643b5e0fe24f44fd
Wose <Jacksonteather@hotmail.com>
2016-12-07 17:28:02 -0500

    Improved drafting and hotspot editing

--- app/components/aframe/hotspot/hotspot_directive.js
+++ app/components/aframe/hotspot/hotspot_directive.js
@@ -1,4 +1,4 @@
-function hotSpot($state, SceneResource) {
+function hotSpot($state, FeatureResource) {
   'ngInject';
   return {
     restrict: 'A',
@@ -8,14 +8,28 @@ function hotSpot($state, SceneResource) {
     require: '^aframeScene',
     templateUrl: 'aframe/hotspot/_hotspot.html',
 		link(scope, elem, attrs, SceneCtrl) {
+			const hotspotId = scope.hotSpot._id;
+			//  and handlers: success...
+			const viewHotspotHandler = data => {
+				document.getElementById(`hotSpot_${hotspotId}`).emit('goto');
+				console.log(data);
+			}
+			// ...and error
+			const errorHandler = error => {
+			  const locals = {
+					type: 'Hotspot load error!',
+					message: 'Looks like this hotspot is broken – darn.'
+		  	}
+				$ErrorReporter.error({ locals });
+			}
 			//
 			// Listen for clicks on scene link element
 			const elemClick =  event => {
+				// const { position, desc, feature } = scope.hotSpot;
 				// If there's a right click active...
 				if (SceneCtrl._rightClick && SceneCtrl._editable) {
 					// ...open the scene link editor:
-					const { position, content, feature } = scope.hotSpot;
-					SceneCtrl.openEditor(SceneCtrl._rightClick, { position, content, feature });
+					SceneCtrl.openEditor(SceneCtrl._rightClick, scope.hotSpot, SceneCtrl.hotSpots);
 				} else {
 					// ...otherwise just use the default scene link behavior
 					// const sceneId = scope.hotSpot.scene;
@@ -23,7 +37,11 @@ function hotSpot($state, SceneResource) {
 					// 	document.getElementById(`link_${sceneId}`).emit('goto');
 					// 	$state.go('scene', { building: data.parent.code, scene: data.code });
 					// }
-					// SceneResource.get({ id: sceneId }).$promise.then(gotoSceneHandler);
+					if (scope.hotSpot.linked && scope.hotSpot.feature) {
+						FeatureResource.get({ id: scope.hotSpot.feature }).$promise.then(viewHotspotHandler, errorHandler);
+					} else {
+						viewHotspotHandler(scope.hotSpot);
+					}
 				}
 			};
 			elem.on('click', elemClick);
