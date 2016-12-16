608cc5c343d373261bd71cebf9ba00839f9778b1
Wose <Jacksonteather@hotmail.com>
2016-12-16 17:14:45 -0500

    Title bar updates
    CTA button changes

--- app/components/titlebar/titlebar_constant.js
+++ app/components/titlebar/titlebar_constant.js
@@ -1,4 +1,25 @@
 const TITLEBAR_OPTS = {
+	register: {
+		active: true,
+		label: 'Register now',
+		tooltip: false,
+		icon: ['person_add', 'person_add'],
+		class: 'cta-button bg-pink'
+	},
+	schedule: {
+		active: true,
+		label: 'Schedule visit',
+		tooltip: false,
+		icon: ['add_alarm', 'add_alarm'],
+		class: 'cta-button bg-green'
+	},
+	apply: {
+		active: true,
+		label: 'Apply online',
+		tooltip: false,
+		icon: ['assignment_turned_in', 'assignment_turned_in'],
+		class: 'cta-button bg-orange'
+	},
 	condense: {
 		active: true,
 		label: 'Toolbar size',
