3ad65dc82aa26421cc7a4cc6a45db5db5b04a491
Wose <Jacksonteather@hotmail.com>
2017-01-06 13:32:47 -0500

    Updated 'docs' gulp task

--- gulp/tasks/docs.js
+++ gulp/tasks/docs.js
@@ -1,19 +1,9 @@
 'use strict';
-
-// **** NOTE: ****
-// This task should be run using Node v4.3.2
-
-import config from '../config';
 import gulp from 'gulp';
-import sia from 'sia';
-import * as pkg from '../../package.json'
+import gulpDocumentation from 'gulp-documentation';
 
-sia(gulp, {
-  basePath: '../../',
-  moduleTitle: 'UOIT Virtual Tour',
-  modulePrefix: 'app',
-  ngVersion: '1.5.8',
-  moduleJs: [config.scripts.src],
-  moduleCss: [config.styles.src],
-  repositoryUrl: pkg.repository && pkg.repository.url.replace(/^git/, 'https').replace(/(\.git)?\/?$/,'')
+gulp.task('docs', function () {
+  return gulp.src('./app/components/**/*.js')
+    .pipe(gulpDocumentation('html'))
+    .pipe(gulp.dest('docs'));
 });
