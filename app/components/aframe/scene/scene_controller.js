38eb262a003cf5fe9ce3ec9b9aae5adba74d8662
Wose <Jacksonteather@hotmail.com>
2017-01-15 00:32:36 -0500

    Added more scene component docs
    Regenerate doc pages

--- app/components/aframe/scene/scene_controller.js
+++ app/components/aframe/scene/scene_controller.js
@@ -1,4 +1,3 @@
-/** @memberOf app.components.aframe.scene */
 import { isDefined, equals } from 'angular';
 
 /**
@@ -7,32 +6,54 @@ import { isDefined, equals } from 'angular';
  * the DOM elements of the scene and updating them when the
  * $aframeScene service changes.
  * 
- * @namespace SceneCtrl
  * @memberOf app.components.aframe.scene
- * @param {object} $scope       Current scope
- * @param {object} $element     <aframe-scene> outer elements
- * @param {object} $aframeScene Scene service dependency
  */
-function SceneCtrl($scope, $element, $aframeScene, $aframeSky) {
-	'ngInject';
+class SceneCtrl {
+	/**
+	 * Initializes the SceneCtrl's dependencies
+	 * @param  {object} $scope       The current scope
+	 * @param  {jqlite} $element     The component's <aframe-scene> element
+	 * @param  {object} $aframeScene The scene service
+	 * @param  {object} $aframeSky   The sky service
+	 */
+	constructor($scope, $element, $aframeScene, $aframeSky) {
+		'ngInject';
+		this.$scope = $scope;
+		this.$element = $element;
+		this.$aframeScene = $aframeScene;
+		this.$aframeSky = $aframeSky;
+	}
 
   /**
-	 * Store JQLite-wrapped <a-scene> and <a-assets>
-	 * Init internal check for right click event
-   *
-   * @memberOf SceneCtrl
-   * @return {void}		No return
+   * Lifecycle hook to initialize component properties; sets
+   * a boolean to hold the rightclick-active flag (for when a
+   * user mouseups the scene with right button) and a string
+   * for the current active scene's ID.
+   * 
+	 * Also stores JQLite-wrapped <a-scene> and <a-assets> for
+	 * appending child elements into the scene from inner components.
    */
-  this.$onInit = () => {
+  $onInit() {
 		this._rightClick = false;
 		this._currentSceneId = '';
 
-		this.$sceneEl = $element.find('a-scene');
+		this.$sceneEl = this.$element.find('a-scene');
 		this.$assetsEl = this.$sceneEl.find('a-assets');
-		this.$aframeScene = $aframeScene;
   }
 
-	const setScene = scene => {
+  /**
+   * Extracts an incoming scene object's relevant data (`id`, `sceneLinks`,
+   * and `hotSpots`) and assigns them to the controller for rendering
+   * on the component.
+   *
+   * If the server-side editor component is attached (i.e. admin user is
+   * logged in), it will have mounted a `checkForDraft()` method on the
+   * controller, which is called here to announce if a newly-navigated-to
+   * scene has saved drafts.
+   * 
+   * @param {object} scene A scene object from API/database
+   */
+	setScene(scene) {
 		
 		const { sceneLinks, hotSpots, _id } = scene;
 
@@ -44,29 +65,37 @@ function SceneCtrl($scope, $element, $aframeScene, $aframeSky) {
 	}
 
   /**
-   * Checks $aframeScene's current scene object each digest
-   * Proceeds if scene is defined; calls setScene()
+   * Lifecycle hook to run a check on every digest loop cycle.
    * 
-   * @memberOf SceneCtrl
-   * @return {void}				No return
+   * Checks $aframeScene's current scene object; proceeds if
+   * scene is defined and calls `setScene()` on the incoming data.
+   *
+   * Checks if there is a sky active on $aframeSky, and if so, if
+   * it is different from the current active sky. Sets the conroller's
+   * sky if they differ or if the controller has no sky.
+   * 
    */
-  this.$doCheck = () => {
-  	if (isDefined($aframeScene.scene)) {
-  		if ($aframeScene.scene._id && $aframeScene.scene._id !== this._currentSceneId) {
-			// if (!this.sky || $aframeScene.sky !== this.sky) {
-				// this.sky = $aframeScene.sky;
-	  		setScene($aframeScene.scene);
+  $doCheck() {
+  	if (isDefined(this.$aframeScene.scene)) {
+  		if (this.$aframeScene.scene._id && this.$aframeScene.scene._id !== this._currentSceneId) {
+			// if (!this.sky || this.$aframeScene.sky !== this.sky) {
+				// this.sky = this.$aframeScene.sky;
+	  		setScene(this.$aframeScene.scene);
 	  	}
   	} else return;
-  	if ($aframeSky.sky) {
-			if (!this.sky || $aframeSky.sky !== this.sky) {
-				this.sky = $aframeSky.sky;
+  	if (this.$aframeSky.sky) {
+			if (!this.sky || this.$aframeSky.sky !== this.sky) {
+				this.sky = this.$aframeSky.sky;
 				console.log('scene controller detects change in sky service', this.sky);
 	  	}
   	}
   }
 
-  this.$onDestroy = () => {
+  /**
+   * Lifecycle cleanup method; unattaches event listener
+   * for context menu (right click) events from the scene.
+   */
+  $onDestroy() {
 		this.$sceneEl.off('contextmenu', contextMenu);
   }
 }
