c59f0548dd8fa0bf7831da4985a3a4cae3240ac6
Wose <Jacksonteather@hotmail.com>
2017-03-22 23:49:44 -0400

    Moved titlebar and buttonbar to components

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,8 +1,6 @@
 import angular from 'angular';
 
-import ButtonBar from './buttonbar/buttonbar';
 import Popup from './popup/popup';
-import TitleBar from './titlebar/titlebar';
 import User from './user/user';
 import Util from './util/util';
 
@@ -10,9 +8,7 @@ import AframeModule from './aframe/aframe';
 AframeModule.registerAll();
 
 let commonModule = angular.module('app.common', [
-  ButtonBar,
   Popup,
-  TitleBar,
   User,
   Util
 ])
--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,17 +1,21 @@
 import angular from 'angular';
 
 import AFrame from './aframe/aframe';
+import ButtonBar from './buttonbar/buttonbar';
 import Drilldown from './drilldown/drilldown';
 import CampusMap from './map/map';
 import Search from './search/search';
 import Settings from './settings/settings';
+import TitleBar from './titlebar/titlebar';
 
 const componentModule = angular.module('app.components', [
   AFrame,
+  ButtonBar,
   Drilldown,
   CampusMap,
   Search,
-	Settings
+	Settings,
+  TitleBar,
 ])
 
 .name;
