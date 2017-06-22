62fb739a897bd545e60fabfbb15411882f39385e
Wose <Jacksonteather@hotmail.com>
2017-06-22 16:45:15 -0400

    🐛 Upgrade UI-router and fix state transition hooks

--- client/app/app.js
+++ client/app/app.js
@@ -124,6 +124,7 @@ angular.module('app', [
   $stateProvider
     .state('home', {
       template: '<ui-view></ui-view>'
+      redirectTo: 'locations',
     });
 
   $urlRouterProvider.otherwise('/');
@@ -143,7 +144,7 @@ angular.module('app', [
   });
 })
 
-.run(($rootScope, $state, $compile, $mdUtil, $popupWindow, UserAuth, AUTH_EVENTS) => {
+.run(($rootScope, $state, $transitions, $compile, $mdUtil, $popupWindow, UserAuth, AUTH_EVENTS) => {
   'ngInject';
 
   $rootScope.$on('cfpLoadingBar:started', event => $mdUtil.nextTick( tick => $compile(
@@ -168,32 +169,23 @@ angular.module('app', [
     return $popupWindow.warning({locals});
   });
 
-  $rootScope.$on('$stateChangeStart', (event, toState, toParams, fromState, fromParams, options) => { 
-    // console.log('STATE CHANGE START', toState, toParams, fromState, fromParams, options);
-    if (toState.data && toState.data.roles && !UserAuth.isAuthorized(toState.data.roles)) {
-      event.preventDefault();
-      if (UserAuth.isAuthenticated()) {
-        // user is not allowed
-        $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
-      } else {
-        // user is not logged in
-        $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
-      }
-    }
+  $transitions.onStart({}, function(trans) {
+    const Auth = trans.injector().get('UserAuth');
+    let toState = trans.$to(),
+    		fromState = trans.$from();
+    console.log('[$transitions] onStart', trans, toState, fromState);
+
+    if (toState.data && toState.data.roles && !Auth.isAuthorized(toState.data.roles)) {
+	    if (auth.isAuthenticated()) {
+	      $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
+	    } else {
+	      $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
+	    }
+	  }
   });
 
-  // change page title based on state
-  $rootScope.$on('$stateChangeSuccess', (event, toState, toParams, fromState, fromParams) => {
-    $rootScope.pageTitle = 'UOIT Virtual Tour';
-    if ( toState.label ) {
-      $rootScope.pageTitle += ' | ';
-      $rootScope.pageTitle += toState.label;
-    }
-    // console.log('STATE CHANGE SUCCESS', toState, toParams, fromState, fromParams);
-  });
-
-  $rootScope.$on('$stateChangeError', (event, toState, toParams, fromState, fromParams, error) => {
-    console.log('STATE CHANGE ERROR: ', error, toState, toParams, fromState, fromParams);
+  $state.defaultErrorHandler(error => {
+    console.log('[$state] defaultErrorHandler', error);
     const locals = {
       type: 'Navigation issue!',
       message: error.message || error.msg || 'The view you requested hit a snag while loading.',
@@ -202,14 +194,15 @@ angular.module('app', [
     $popupWindow.error({locals});
   });
 
-  $rootScope.$on('$stateNotFound', (event, unfoundState, fromState, fromParams) => { 
+  $transitions.onError({}, function(trans) {
+    const Popup = trans.injector().get('$popupWindow');
+    console.log('[$transitions] onError', trans.error);
     const locals = {
       type: 'Page not found!',
-      message: `The page associated with (${unfoundState.to} : ${unfoundState.toParams}) cannot be located.`,
+      message: `The page associated with "${trans.$to()}" cannot be located.`,
       suggest: [3]
     }
-    $popupWindow.error({locals});
-    console.log('STATE NOT FOUND', unfoundState, fromState, fromParams);
+    Popup.error({locals});
   });
 
   console.log('starting auth init...')
