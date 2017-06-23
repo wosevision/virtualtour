4dcb58456354d026b7a9295f97a980baac9df556
Wose <Jacksonteather@hotmail.com>
2017-06-23 12:37:00 -0400

    🐛 Fix drilldown show/hide toggling

--- client/app/components/drilldown/drilldown.component.js
+++ client/app/components/drilldown/drilldown.component.js
@@ -11,9 +11,9 @@ export const DrilldownComponent = {
 			class="drilldown-item"
 			ng-repeat="item in $ctrl.children"
 			ng-click="$ctrl.toggle($event, item, $ctrl.level)"
-			ng-class="{ 'is-active': item._active, 'is-hidden': item.hidden }"
+			ng-class="{ 'is-active': item._active, 'is-hidden': item._hidden }"
 			md-ink-ripple="{{ item._active ? false : '#003C71' }}">
-			<div ng-if="!item.hidden" style="padding-right: 30px;" class="drilldown-content" ng-class="{'md-caption': item.active, 'md-title': !item.active, 'selected': item.selected }">
+			<div ng-if="!item._hidden" style="padding-right: 30px;" class="drilldown-content" ng-class="{'md-caption': item._active, 'md-title': !item._active, 'selected': item.selected }">
 				<span>{{ ::item.name }}</span>
 				<div class="open-indicator"></div>
 			</div>
@@ -25,11 +25,26 @@ export const DrilldownComponent = {
   		'ngInject';
   		this.$state = $state;
   	}
+  	toggleAll(except) {
+  		this.children.forEach(child => {
+				child._active = (child === except);
+				child._hidden = !(child === except);
+  		});
+  	}
+  	closeAll() {
+  		this.children.forEach(child => {
+				child._active = child._hidden = false;
+  		});
+  	}
   	toggle(event, item, level) {
 			event.stopPropagation();
-			item._active = !item._active;
-			this.$state.go(item._level, item._params);
   		console.log('[drilldown.controller] toggle', item, level)
+			if (!item._active) {
+				this.toggleAll(item);
+				this.$state.go(item._level, item._params);
+			} else {
+				this.closeAll();
+			}
   	}
   	// $onInit() {
   	// 	console.log('[drilldown.controller] $onInit', this)
