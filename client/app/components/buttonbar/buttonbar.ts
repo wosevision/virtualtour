734273c7f419a0a80b51631547e1ff0c7e0f0526
Wose <Jacksonteather@hotmail.com>
2017-06-26 13:14:47 -0400

    🚿 Separate buttonbar views constant

--- client/app/components/buttonbar/buttonbar.ts
+++ client/app/components/buttonbar/buttonbar.ts
@@ -3,6 +3,7 @@ import ngMaterial from 'angular-material';
 import uiRouter from 'angular-ui-router';
 
 import { ButtonbarComponent } from './buttonbar.component';
+import { BUTTONBAR_VIEWS } from './buttonbar-views.constant';
 
 import './buttonbar.scss';
 
@@ -13,32 +14,6 @@ export const ButtonbarModule = angular.module('buttonbar', [
 
 .component('buttonBar', ButtonbarComponent)
 
-.constant('BUTTONBAR_VIEWS', {
-  locations: {
-    label: 'By location',
-    icon: 'pin_drop',
-    desc: 'View a list of available tour locations'
-  },
-  map: {
-    label: 'By map',
-    icon: 'map',
-    desc: 'Explore locations from the UOIT Campus Map'
-  },
-  // guided: {
-  //   label: 'Guided tour',
-  //   icon: 'rotate_90_degrees_ccw',
-  //   desc: 'Take a preset trip with a video tour guide'
-  // },
-  search: {
-    label: 'Search',
-    icon: 'search',
-    desc: 'Find specific Virtual Tour highlights'
-  },
-  settings: {
-    label: 'Settings',
-    icon: 'tune',
-    desc: 'Adjust the Virtual Tour experience'
-  }
-})
+.constant('BUTTONBAR_VIEWS', BUTTONBAR_VIEWS)
 
 .name;
